b670523357b1761a0834e3661de5a8f4213d1f71
HyeBinYang <skdisk7368@gmail.com>
2021-10-25 04:31:14 +0900

    Add RightSide

--- src/components/home/RightSide.jsx
+++ src/components/home/RightSide.jsx
@@ -4,7 +4,35 @@ import "./RightSide.css";
 function RightSide() {
   return (
     <div id="rightside">
-      <h1>Right</h1>
+      <div className="rightside__user">
+        <i class="fas fa-seedling"></i>
+        <span>skdisk3895</span>
+      </div>
+      <div className="rightside__recommend">
+        <h3>회원님을 위한 추천 영화</h3>
+        <div className="recommend__movies">
+          <div className="movie">
+            <img src="logo192.png" alt="" />
+            <span>포레스트 검프</span>
+          </div>
+          <div className="movie">
+            <img src="logo192.png" alt="" />
+            <span>포레스트 검프포레스트 검프포레스트 검프</span>
+          </div>
+          <div className="movie">
+            <img src="logo192.png" alt="" />
+            <span>포레스트 검프</span>
+          </div>
+          <div className="movie">
+            <img src="logo192.png" alt="" />
+            <span>포레스트 검프</span>
+          </div>
+          <div className="movie">
+            <img src="logo192.png" alt="" />
+            <span>포레스트 검프</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
