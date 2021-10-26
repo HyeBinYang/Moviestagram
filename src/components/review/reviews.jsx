9128da0079eba22ef4364f1f332ea33d8195e59d
HyeBinYang <skdisk7368@gmail.com>
2021-10-26 16:01:26 +0900

    Add Reviews component

--- src/components/review/reviews.jsx
+++ src/components/review/reviews.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import "./Reviews.css";
 
 export default function reviews() {
   return (
-    <div>
-      <h1>Reviews</h1>
+    <div id="reviews">
+      <img src="logo512.png" alt="Review image" className="review" />
+      <img src="logo512.png" alt="Review image" className="review" />
+      <img src="logo512.png" alt="Review image" className="review" />
+      <img src="logo192.png" alt="Review image" className="review" />
+      <img src="logo512.png" alt="Review image" className="review" />
     </div>
   );
 }
